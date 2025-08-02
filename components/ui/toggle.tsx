543bb63aa2d082f37940201bb52bbdf83b15edc7
Irkham <121574317+nirwanadoteth@users.noreply.github.com>
2025-08-02 23:38:01 +0700

    Enhance Toggle component to include 'aria-label' prop for improved accessibility

--- components/ui/toggle.tsx
+++ components/ui/toggle.tsx
@@ -31,7 +31,9 @@ const toggleVariants = cva(
 const Toggle = React.forwardRef<
 	React.ElementRef<typeof TogglePrimitive.Root>,
 	React.ComponentPropsWithoutRef<typeof TogglePrimitive.Root> &
-		VariantProps<typeof toggleVariants>
+		VariantProps<typeof toggleVariants> & {
+			'aria-label': string;
+		}
 >(({ className, variant, size, ...props }, ref) => (
 	<TogglePrimitive.Root
 		ref={ref}
